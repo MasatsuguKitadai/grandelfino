009ae4de0eee87c04145ad284a836c573f8f4000
Laptop_WSL <b8123026@gmail.com>
2023-04-22 11:09:08 +0900

    html

--- programs/skidpad/01_simulation.cpp
+++ programs/skidpad/01_simulation.cpp
@@ -16,19 +16,18 @@
 #include <sstream>
 using namespace std;
 
-#include "../hpp/class.hpp"
-#include "../hpp/functions.hpp"
-
 /** パラメータ **/
 const float r = 8.0;  // 旋回半径 [m]
 const float v = 40.0; // 走行速度 [km/h]
-const int n = 4.0;    // 周回数 [-]
+const float hz = 100; // サンプリング周期 [Hz]
+const float n = 4.0;  // 周回数 [-]
 
 /** パラメータ（自動的に決まる） **/
-const float v2 = v * 1000.0 / 3600.0;    // 走行速度 [m/s]
-const float omega = v2 / r;              // 角速度 [rad/s]
-const float pi = 4 * atan(1.0);          // 円周率 [rad]
-const float distance = 2.0 * pi * r * n; // 走行距離 [m]
+const float v2 = v * 1000.0 / 3600.0;   // 走行速度 [m/s]
+const float omega = v2 / r;             // 角速度 [rad/s]
+const float pi = 4 * atan(1.0);         // 円周率 [rad]
+const float mileage = 2.0 * pi * r * n; // 走行距離 [m]
+const float sec_max = mileage / v2;     // 最大時刻 [s]
 
 /** プロトタイプ宣言 **/
 float Skidpad_x(float sec);
@@ -41,6 +40,20 @@ void Gnuplot();
 /**************************************************************/
 int main()
 {
+    /** 配列 **/
+    vector<float> sec(sec_max * hz);
+    vector<float> x(sec_max * hz);
+    vector<float> y(sec_max * hz);
+
+    /** 車両位置の計算 **/
+    for (int i = 0; i < sec_max * hz; i++)
+    {
+        sec[i] = 1.0 / hz * i;
+        x[i] = Skidpad_x(i);
+        y[i] = Skidpad_y(i);
+        printf("%.3f\t[s]\tx = %.3f\ty = %.3f\n", sec[i], x[i], y[i]);
+    }
+
     return 0;
 }
 
@@ -57,7 +70,7 @@ float Skidpad_x(float sec)
     float x;
     if (0 <= theta && theta <= 2.0 * pi * 2.0)
     {
-        x = r * cos(theta) + r;
+        x = -1.0 * r * cos(theta) + r;
     }
     else if (2.0 * pi * 2.0 <= theta)
     {
